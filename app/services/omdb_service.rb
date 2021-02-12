00ecebfedbed3633b34d07d0f582c12b8621e209
muratrahmikurtul <muratrahmikurtul@gmail.com>
2021-02-12 17:17:25 +0100

    add begin rescue

--- app/services/omdb_service.rb
+++ app/services/omdb_service.rb
@@ -1,13 +1,18 @@
 class OmdbService
   def call_service(prm)
-    response = Omdb::Api.search_movies_by_name(prm)
-    @movies = []
-    if response
-      response = response.sort_by { |h| h['Year']  }
-      response.map do |movie|
-        @movies.push(%W[#{movie["Title"]} #{movie["Year"]} #{movie["Poster"]}])
+    begin
+      response = Omdb::Api.search_movies_by_name(prm)
+      @movies = []
+      if response
+        response = response.sort_by { |h| h['Year']  }
+        response.map do |movie|
+          @movies.push(%W[#{movie["Title"]} #{movie["Year"]} #{movie["Poster"]}])
+        end
       end
+      @movies
+    rescue StandardError => e
+      Rails.logger.error("#{self.class.name}.#{__method__} Exception: #{e.message} #{e.backtrace}")
+      return false
     end
-    @movies
   end
 end
